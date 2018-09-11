2e0c633d3ed55c687502f4ed9dbd3b260f787d85
alanpramuk9 <alanpramuk9@gmail.com>
2018-09-11 03:14:02 -0500

    meh. another dumb commit.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,11 @@
 let express = require('express');
 const cors = require('cors');
-<<<<<<< HEAD
 const wordsRouter = require('./words');
 const scoresRouter = require('./scores');
 
 let router = express.Router();
 
 router.use('/words', wordsRouter);
-=======
-const scoresRouter = require('./scores');
-
-let router = express.Router();
->>>>>>> 0cb4026d4afd35b927e5b9b6345dd1b10cbb10d5
 router.use('/scores', scoresRouter);
 
 module.exports = router;
